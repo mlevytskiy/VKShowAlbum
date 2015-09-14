d3b1202dce086f87eb83ec688df1f626c781bce7
mlevytskiy <m.levytskiy@gmail.com>
2015-09-14 18:21:08 +0300

    fix problems with change password

--- app/src/main/java/vkshowalbum/mlevytskiy/com/vkshowalbum/App.java
+++ app/src/main/java/vkshowalbum/mlevytskiy/com/vkshowalbum/App.java
@@ -1,7 +1,10 @@
 package vkshowalbum.mlevytskiy.com.vkshowalbum;
 
 import android.app.Application;
+import android.content.*;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
 
@@ -12,9 +15,22 @@ public class App extends Application {
 
     public static final String[] VK_SCOPE = new String[]{VKScope.PHOTOS};
 
+    VKAccessTokenTracker vkAccessTokenTracker = new VKAccessTokenTracker() {
+        @Override
+        public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
+            if (newToken == null) {
+                VKSdk.logout();
+                Intent intent = new Intent(App.this, AlbumsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        }
+    };
+
     public void onCreate() {
         super.onCreate();
         VKSdk.initialize(this);
+        vkAccessTokenTracker.startTracking();
     }
 
 }
